af413fe18c6642506ca9b16bec66c8872fe870be
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-01-04 13:13:44 +0600

    navbar responciv update

--- Components/Shared/Navbar.tsx
+++ Components/Shared/Navbar.tsx
@@ -3,16 +3,17 @@ import { FaChevronDown } from "react-icons/fa";
 import { FaRegUser } from "react-icons/fa";
 import { FiShoppingCart } from "react-icons/fi";
 import { FiHeart } from "react-icons/fi";
+import { IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <div className="max-w-screen-lg mx-auto flex justify-between my-5">
+    <div className="w-11/12 sm:w-10/12 mx-auto flex justify-between my-5">
        <div className="flex items-center gap-x-3">
         <Image src={"/cat-leg.png"} alt="cat-leg" width={26} height={26} className=""/>
         <h1 className="text-2xl fontLaila text-[#FF6F3F]">PetStick</h1>
        </div>
         
-        <div className="flex items-center">
+        <div className="hidden md:flex items-center ">
             <ul className="flex gap-x-3 text-lg fontLora font-medium">
                 <li><a href="#" className="flex gap-x-2 text-[#FF6F3F] hover:text-green-400 hover:font-extrabold"><span>Home</span><FaChevronDown className="mt-1.5"/></a></li>
                 <li><a href="#" className=" hover:text-green-400 hover:font-extrabold">Pets</a></li>
@@ -23,7 +24,7 @@ const Navbar = () => {
             </ul>
         </div>
 
-        <div className="flex items-center gap-x-10">
+        <div className=" lg:flex xl:flex 2xl:flex items-center gap-x-10 hidden">
             <div className="flex gap-x-3 text-xl">
             <FiHeart />
             <FiShoppingCart />
@@ -31,6 +32,10 @@ const Navbar = () => {
             </div>
             
             <button className="btnStyle ">Register</button>
+        </div>
+
+        <div className="flex items-center text-3xl lg:hidden">
+           <IoMenu />
         </div>
     </div>
     
