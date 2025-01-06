010364c8089a3256f721004ded9f2ebcfb006c52
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-01-06 13:09:49 +0600

    about section working pandding

--- Components/About.tsx
+++ Components/About.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Image from 'next/image'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='w-11/12 mx-auto text-center my-20'>
+     <div>
+      <h1 className=' text-5xl font-bold fontLaila '>Features Categories</h1>
+      <p className=' text-lg font-medium fontLora w-[45%] my-4 mx-auto'>At petstick, access pet listings, shop easily, and book grooming,
+      vet visits, and training all through personalized profiles.</p>
+     </div>
+
+     <div>
+      <div>
+        <div className=' bg-[#FADDD4] w-full h-52'>
+          <Image src={'/cat-leg-center.png'} alt='cat-leg-center' width={32} height={32}/>
+        </div>
+      </div>
+     </div>
+    </div>
   )
 }
 
