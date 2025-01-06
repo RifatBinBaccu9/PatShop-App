ae0605bd1ae6fd58efb1233ffaa20a37370e83f7
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-01-06 16:12:21 +0600

    services working update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/Components/About";
 import Hero from "@/Components/Hero";
+import Services from "@/Components/Services";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
    <div>
     <Hero />
     <About />
+    <Services />
    </div>
   );
 }
