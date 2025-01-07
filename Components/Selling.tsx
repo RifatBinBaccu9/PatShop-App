8332a8c3e3018dd07295c7070039e8caa98f8e04
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-01-07 15:12:28 +0600

    selling responcive desgin done

--- Components/Selling.tsx
+++ Components/Selling.tsx
@@ -5,12 +5,12 @@ import { BsFolder2Open } from "react-icons/bs";
 
 const Selling = () => {
   return (
-    <div>
-        <div className='w-10/12 mx-auto my-20'>
-            <h1 className=' text-5xl font-laila font-bold text-center py-10'>Our Best Selling Product</h1>
+    <div className=' relative'>
+        <div className=' w-9/12 md:w-10/12 mx-auto my-20'>
+            <h1 className=' text-3xl sm:text-5xl font-laila font-bold text-center py-10'>Our Best Selling Product</h1>
            
-           <div className='grid grid-cols-4 gap-4'>
-             <div className=' border rounded-md  p-3'>
+           <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-4'>
+             <div className=' border rounded-md p-3'>
                 <Image src={'/image.png'} alt='image' width={100} height={100} className='w-full'/>
                 <div className='flex justify-between text-lg font-medium mt-2'>
                     <h4>Pedigree adult complete nutrition</h4>
@@ -91,6 +91,11 @@ const Selling = () => {
 
 
            </div>
+        </div>
+
+        <div>
+            <Image src={'/Isolation_Mode.png'} alt='' width={100} height={100} className=' absolute top-0 right-0'/>
+            <Image src={'/Isolation_Moder.png'} alt='' width={100} height={100} className=' absolute bottom-0 left-0'/>
         </div>
     </div>
   )
