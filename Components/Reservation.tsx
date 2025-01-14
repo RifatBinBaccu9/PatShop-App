d2e2df8c39fb79a569d7a11c708e3df2dec3030a
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-01-14 16:53:36 +0600

    reservation

--- Components/Reservation.tsx
+++ Components/Reservation.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Reservation = () => {
   return (
-    <div className='my-[120px]'>
+    <div className='my-[120px] bg-white text-black'>
         <div className="pat-container relative">
       <div
         className="bg-cover bg-center h-[744px] rounded-xl"
@@ -10,91 +10,91 @@ const Reservation = () => {
       >
         <div className="absolute bg-black opacity-50 inset-0 rounded-[20px]"></div>
 
-        <div className="">
-          <div className="">
-            <div className="bg-white rounded-md p-4 py-8 w-full sm:w-11/12 md:w-auto">
-              <h1 className="text-3xl font-bold font-laila text-center">
+        <div className="flex w-full justify-between items-center px-10 py-[78px] gap-[65px]">
+          <div className="bg-white opacity-95 rounded-[20px] w-[534px]">
+            <div className="">
+              <h1 className="text-black text-[28px] font-bold font-laila text-center pt-[60px]">
                 Online reservation
               </h1>
-              <hr className="my-3" />
+              <hr className=" text-[#D9D9D9] border mt-6 mb-10" />
 
-              <div className="grid grid-cols-2 gap-4">
+              <div className="grid grid-cols-2 gap-10 px-6">
                 <div>
-                  <div className="flex text-lg font-medium font-lora gap-2 my-2">
+                  <div className="flex text-lg font-semibold font-lora mb-4">
                     <span>
                       <img src="/Frame.png" alt="" />
                     </span>
                     <span>Pate Type</span>
                   </div>
                   <select
-                    className="border p-3 font-normal text-base rounded-md focus:border-black w-full"
+                    className="w-full p-5 border border-[#D9D9D9] rounded-sm text-base font-normal font-lora"
                   >
-                    <option value="Select Pet Type">Select Pet Type</option>
+                    <option value="Select Pet Type" className='text-base font-normal font-lora'>Select Pet Type</option>
                     <option value="saab">Saab</option>
                     <option value="opel">Opel</option>
                     <option value="audi">Audi</option>
                   </select>
                 </div>
                 <div>
-                  <div className="flex text-lg font-medium font-lora gap-2 my-2">
+                  <div className="flex text-lg font-semibold font-lora mb-4">
                     <span>
                       <img src="/Capa_1.png" alt="" />
                     </span>
                     <span>Pate Type</span>
                   </div>
                   <select
-                    className="border p-3 font-normal text-base rounded-md focus:border-black w-full"
+                    className="w-full p-5 border border-[#D9D9D9] rounded-sm text-base font-normal font-lora"
                   >
-                    <option value="Select Pet Type">1 Pets</option>
+                    <option value="Select Pet Type" className='text-base font-normal font-lora'>Select Pet Type</option>
                     <option value="saab">Saab</option>
                     <option value="opel">Opel</option>
                     <option value="audi">Audi</option>
                   </select>
                 </div>
                 <div>
-                  <div className="flex text-lg font-medium font-lora gap-2 my-2">
+                  <div className="flex text-lg font-semibold font-lora mb-4">
                     <span>
                       <img src="/Frame1.png" alt="" />
                     </span>
                     <span>Check In</span>
                   </div>
                   <select
-                    className="border p-3 font-normal text-base rounded-md focus:border-black w-full"
+                    className="w-full p-5 border border-[#D9D9D9] rounded-sm text-base font-normal font-lora"
                   >
-                    <option value="Select Pet Type">DD-MM-YY</option>
+                    <option value="Select Pet Type" className='text-base font-normal font-lora'>Select Pet Type</option>
                     <option value="saab">Saab</option>
                     <option value="opel">Opel</option>
                     <option value="audi">Audi</option>
                   </select>
                 </div>
                 <div>
-                  <div className="flex text-lg font-medium font-lora gap-2 my-2">
+                  <div className="flex text-lg font-semibold font-lora mb-4">
                     <span>
                     <img src="/Frame1.png" alt="" />
                     </span>
                     <span>Check Out</span>
                   </div>
                   <select
-                    className=" border p-3 font-normal text-base rounded-md focus:border-black w-full"
+                    className="w-full p-5 border border-[#D9D9D9] rounded-sm text-base font-normal font-lora"
                   >
-                    <option value="Select Pet Type">DD-MM-YY</option>
+                    <option value="Select Pet Type" className='text-base font-normal font-lora'>Select Pet Type</option>
                     <option value="saab">Saab</option>
                     <option value="opel">Opel</option>
                     <option value="audi">Audi</option>
                   </select>
                 </div>
               </div>
-              <div className="flex justify-center">
-                <button className="bg-[#FF6F3F] text-white pt-1 pb-[6px] px-5 text-lg font-normal font-lora rounded-3xl mt-4">
+              <div className="flex justify-center mb-[72px] mt-[50px]">
+                <button className="bg-[#FF6F3F] text-white text-lg font-lora font-semibold rounded-[20px] py-4 px-[134px]">
                   Check Availability
                 </button>
               </div>
             </div>
           </div>
 
-          <div className="text-white text-center  md:text-start w-11/12 mx-auto md:w-[45%] lg:w-[55%] mt-10 md:mt-32 lg:mt-[267px]">
-            <h1 className="text-5xl font-laila font-bold mb-3">Pet’s day care</h1>
-            <p className="font-normal text-base mb-10 md:mb-0">
+          <div className="w-[641px] text-white opacity-95">
+            <h1 className="text-5xl font-laila font-bold mb-6">Pet’s day care</h1>
+            <p className="text-lg font-lora font-semibold">
               A petstick pet day care offers a safe, fun place where pets get
               care, play, and attention while their owners are away, keeping
               them happy and stress-free.
