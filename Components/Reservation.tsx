766a41c66d6ab838c486b664ec134be3cb4c277a
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-01-15 10:31:05 +0600

    res

--- Components/Reservation.tsx
+++ Components/Reservation.tsx
@@ -3,22 +3,22 @@ import React from 'react'
 const Reservation = () => {
   return (
     <div className='my-[120px] bg-white text-black'>
-        <div className="pat-container relative">
+        <div className="pat-container ">
       <div
-        className="bg-cover bg-center h-[744px] rounded-xl"
+        className="bg-cover bg-center h-[1300px] md:h-[1000px] xl:h-[744px] rounded-xl relative"
         style={{ backgroundImage: "url('/image1.png')" }}
       >
         <div className="absolute bg-black opacity-50 inset-0 rounded-[20px]"></div>
 
-        <div className="xl:flex w-full justify-between items-center px-10 py-[78px] gap-[65px]">
-          <div className="bg-white opacity-95 rounded-[20px] w-[534px]">
+        <div className="flex flex-col xl:flex-row w-full justify-center xl:justify-between items-center sm:px-10 py-[78px] xl:gap-[65px]">
+          <div className="bg-white opacity-95 rounded-[20px] w-11/12 sm:w-[534px]">
             <div className="">
               <h1 className="text-black text-[28px] font-bold font-laila text-center pt-[60px]">
                 Online reservation
               </h1>
               <hr className=" text-[#D9D9D9] border mt-6 mb-10" />
 
-              <div className="grid grid-cols-2 gap-10 px-6">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-10 px-6">
                 <div>
                   <div className="flex text-lg font-semibold font-lora mb-4">
                     <span>
@@ -85,15 +85,15 @@ const Reservation = () => {
                 </div>
               </div>
               <div className="flex justify-center mb-[72px] mt-[50px]">
-                <button className="bg-[#FF6F3F] text-white text-lg font-lora font-semibold rounded-[20px] py-4 px-[134px]">
+                <button className="bg-[#FF6F3F] text-white text-lg font-lora font-semibold rounded-[20px] py-3 sm:py-4 px-[50px] sm:px-[134px]">
                   Check Availability
                 </button>
               </div>
             </div>
           </div>
 
-          <div className="w-[641px] text-white opacity-95">j
-            <h1 className="text-5xl font-laila font-bold mb-6">Pet’s day care</h1>
+          <div className="w-11/12 md:w-[641px] text-white opacity-95 pt-20 xl:pt-0">
+            <h1 className="text-4xl sm:text-5xl font-laila font-bold mb-6">Pet’s day care</h1>
             <p className="text-lg font-lora font-semibold">
               A petstick pet day care offers a safe, fun place where pets get
               care, play, and attention while their owners are away, keeping
