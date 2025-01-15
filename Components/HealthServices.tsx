d2ab8d60e7af6287660fdf03768e15cc24c7856a
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-01-15 21:18:50 +0600

    health update

--- Components/HealthServices.tsx
+++ Components/HealthServices.tsx
@@ -3,77 +3,81 @@ import { FaArrowRightLong } from "react-icons/fa6";
 
 const HealthServices = () => {
   return (
-    <div className="py-30">
-        <div className='w-11/12 sm:w-8/12 md:w-10/12 mx-auto lg:flex gap-10'>
-            <div className=" lg:w-[45%]">
-               <h1 className='text-3xl sm:text-5xl font-laila font-bold mb-3'>Health and Wellness Services</h1>
-               <div className="lg:w-10/12 ">
-               <p className='text-base font-medium'>Designed so animals can relax & indulge in pet stylists, run around our exercise yards, make new friends.</p>
+    <div className="mb-[172px] mt-[120px]">
+        <div className='pat-container 2xl:flex gap-6'>
+            <div className="w-full 2xl:w-[495px] mt-[40px]">
+               <h1 className='text-3xl sm:text-5xl font-laila font-bold'>Health and Wellness Services</h1>
+               <div className="w-full 2xl:w-[424px] mt-6">
+               <p className=' text-base font-lora font-normal'>Designed so animals can relax & indulge in pet stylists, run around our exercise yards, make new friends.</p>
 
-                <div className='flex gap-8 text-lg font-laila font-semibold mt-5'>
-                 <h3>For Dog</h3>
+                <div className='flex text-lg font-laila font-semibold mt-10 mb-14 gap-10'>
+                 <h3 className="text-[#FF6F3F]">For Dog</h3>
                  <h3>For Cat</h3>
                  <h3>For Others</h3>
                 </div>
                 
                 <div className="mt-5">
-                 <div className="bg-[#FF6F3F] rounded-sm flex justify-between border p-3 text-2xl font-laila font-medium mb-3">
+                 <div className="bg-[#FF6F3F] rounded-r-lg flex justify-between text-2xl font-laila font-medium p-3 mb-6 border border-[#D9D9D9]">
                      <p>Dog Regular Checkup</p>
                      <FaArrowRightLong />
                  </div>
-                 <div className="flex justify-between rounded-sm border p-3 text-2xl font-laila font-medium mb-3">
+                 <div className="rounded-r-lg flex justify-between text-2xl font-laila font-medium p-3 mb-6 border border-[#D9D9D9]">
                      <p>Dog Dental Cear</p>
                      <FaArrowRightLong />
                  </div>
-                 <div className="flex justify-between rounded-sm border p-3 text-2xl font-laila font-medium mb-3">
+                 <div className="rounded-r-lg flex justify-between text-2xl font-laila font-medium p-3 mb-6 border border-[#D9D9D9]">
                      <p>Dog Nutrition</p>
                      <FaArrowRightLong />
                  </div>
-                 <div className="flex justify-between rounded-sm border p-3 text-2xl font-laila font-medium mb-3">
+                 <div className="rounded-r-lg flex justify-between text-2xl font-laila font-medium p-3 mb-6 border border-[#D9D9D9]">
                      <p>Dog Vaccination</p>
                      <FaArrowRightLong />
                  </div>
                 </div>
-                <button className="btnStyle mt-5">Make an Appointment</button>
+                <button className="btnStyle mt-[50px]">Make an Appointment</button>
                </div>
             </div>
 
-            <div className="lg:w-[55%] flex justify-center mt-20 lg:mt-0">
+            <div className="w-full 2xl:w-[800px] flex justify-center items-center pt-32 2xl:mt-0">
             <div>
-            <div className="flex justify-center mb-16 relative">
-            <div className="w-full sm:w-[65%] border-2 border-dashed rounded-md bg-[#F4E1D3] p-3 sm:p-5 ">
-                  <div className="w-full sm:w-11/12  ">
+
+            <div className="flex justify-center relative mb-[86px] ">
+            <div className=" border-2 border-dashed border-[#44433F] rounded-md bg-[#D0F9FF] ">
+                  <div className=" py-10  pl-10 pr-10 sm:pr-20">
                   <h1 className='text-lg font-laila font-semibold'>Dog Regular Checkup</h1>
-                  <hr className=" border my-2 w-11/12"/>
-                  <p className=''>Regular checkups keep your dog healthy and protected.</p>
+                  <hr className=" border border-[#44433F] w-[240px] mt-6 mb-4"/>
+                  <p className='text-base font-lora font-normal w-[240px]'>Regular checkups keep your dog healthy and protected.</p>
                   </div>
                 </div>
-                  <Image src={'/Health1.png'} alt="Health1.png" width={100} height={100} className="w-[35%] sm:absolute sm:-top-7 sm:-right-8"/>
+                  <Image src={'/Health1.png'} alt="Health1.png" width={100} height={100} className="w-[300px] absolute -top-[80px] right-1 hidden lg:block"/>
             </div>
-            <div className="flex justify-start mb-16 relative">
-                <div className="w-full sm:w-[65%] border-2 flex justify-end border-dashed rounded-md bg-[#FFE1B5] p-3 sm:p-5 ">
-                  <div className="w-full sm:w-[80%]  flex justify-end">
+
+
+            <div className="flex justify-start relative mb-[86px] sm:ml-[130px]">
+                <div className="border-2 border-dashed border-[#44433F] rounded-md bg-[#FFE1B5]">
+                  <div className=" flex justify-end pt-[50px] pb-[53px] pr-10 pl-10 sm:pl-[80px]">
                   <div>
                   <h1 className='text-lg font-laila font-semibold'>Behavioral Assessment</h1>
-                  <hr className=" border my-2 w-11/12"/>
-                  <p className=''>Address any changes in behavior
+                  <hr className="w-[240px] border border-[#44433F] mt-[26px] mb-4"/>
+                  <p className='w-[240px]'>Address any changes in behavior
                   or mobility</p>
                   </div>
                   </div>
                 </div>
-                  <Image src={'/Health2.png'} alt="Health1.png" width={100} height={100} className="w-[30%] sm:absolute sm:-top-7 sm:-left-20"/>
-                <Image src={'/timer.png'} alt="timer" width={100} height={100} className="w-1/3 h-[100px] mt-5 hidden sm:block"/>
+                  <Image src={'/Health2.png'} alt="Health1.png" width={100} height={100} className="w-[242px] h-[300px] lg:absolute -top-[50px] -left-[150px] hidden lg:block"/>
+                <Image src={'/timer.png'} alt="timer" width={100} height={100} className="w-[300px] h-[187px] mt-5 ml-5 hidden lg:block"/>
             </div>
-            <div className="flex justify-center mb-16 relative">
-                <div className="w-full sm:w-[65%] border-2 border-dashed rounded-md bg-[#F4E1D3] p-3 sm:p-5 ">
-                  <div className="w-full sm:w-11/12  ">
-                  <h1 className='text-lg font-laila font-semibold'>Dog Day Care Trips</h1>
-                  <hr className=" border my-2 w-11/12"/>
-                  <p className=''>Monitor weight and adjust diet
-                  if needed.</p>
+
+
+            <div className="flex justify-center relative ">
+            <div className=" border-2 border-dashed border-[#44433F] rounded-md bg-[#F4E1D3] ">
+                  <div className=" py-10  pl-10 pr-10 sm:pr-20">
+                  <h1 className='text-lg font-laila font-semibold'>Dog Regular Checkup</h1>
+                  <hr className=" border border-[#44433F] w-[240px] mt-6 mb-4"/>
+                  <p className='text-base font-lora font-normal w-[240px]'>Regular checkups keep your dog healthy and protected.</p>
                   </div>
                 </div>
-                  <Image src={'/Health3.png'} alt="Health1.png" width={100} height={100} className="w-[35%] sm:absolute sm:-top-7 sm:-right-8"/>
+                  <Image src={'/Health3.png'} alt="Health1.png" width={100} height={100} className="w-[300px] hidden lg:block lg:absolute -top-[80px] right-1"/>
             </div>
             
            
